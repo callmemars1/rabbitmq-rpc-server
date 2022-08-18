1c002632553855814406bf627e333d8cf5edc819
Sergey Martynov <callmemars111@gmail.com>
2022-08-18 13:57:40 +0300

    added ToUri method in RabbitMqConnectionProperties and added tests for this method

---
+++ Smartynov.RabbitMq.Rpc.Tests/RabbitMqConnectionProperties_Test.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection.PortableExecutable;
+using Smartynov.RabbitMq.Rpc;
+
+namespace Smartynov.RabbitMq.Rpc.Tests;
+
+[TestFixture]
+public class RabbitMqConnectionPropertiesTest
+{
+    [Test]
+    [TestCase(
+        true, 
+        "sub.domain.com", 
+        "myusername", 
+        "mypassword", 
+        null, 
+        null, 
+        "amqps://myusername:mypassword@sub.domain.com"
+        )
+    ]
+    [TestCase(
+        false, 
+        "178.228.54.1", 
+        "myusername", 
+        "mypassword", 
+        "myvhost", 
+        56, 
+        "amqp://myusername:mypassword@178.228.54.1:56/myvhost"
+        )
+    ]
+    public void ToUri_Test
+    (
+        bool secure,
+        string hostname,
+        string username,
+        string password,
+        string? virtualhost,
+        int? port,
+        Uri result
+    )
+    {
+        var properties = new RabbitMqConnectionProperties
+        {
+            Secure = secure,
+            Hostname = hostname,
+            Username = username,
+            Password = password,
+            VirtualHost = virtualhost,
+            Port = port,
+        };
+        var uri = properties.ToUri();
+        Assert.That(uri, Is.EqualTo(result));
+    }
+}
--- Smartynov.RabbitMq.Rpc/RabbitMqConnectionProperties.cs
+++ Smartynov.RabbitMq.Rpc/RabbitMqConnectionProperties.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Channels;
+﻿using System.Text;
+using System.Threading.Channels;
 using RabbitMQ.Client;
 
 namespace Smartynov.RabbitMq.Rpc;
@@ -8,11 +9,27 @@ public record RabbitMqConnectionProperties
     public RabbitMqConnectionProperties()
     {
     }
+
+    public bool Secure { get; init; } = false;
     public string Hostname { get; init; } = "localhost";
     public string Username { get; init; } = "guest";
     public string Password { get; init; } = "guest";
-    
     public string? VirtualHost { get; init; } = null!;
-    
-    public int Port { get; init; } = 5672;
+    public int? Port { get; init; } = 5672;
+
+    public Uri ToUri()
+    {
+        var uriBuilder = new StringBuilder();
+        uriBuilder.Append("amqp");
+        if (Secure) uriBuilder.Append('s');
+        uriBuilder.Append("://");
+        uriBuilder.Append(Username);
+        uriBuilder.Append(':');
+        uriBuilder.Append(Password);
+        uriBuilder.Append('@');
+        uriBuilder.Append(Hostname);
+        if (Port.HasValue) uriBuilder.Append(':').Append(Port.Value);
+        if (VirtualHost != null) uriBuilder.Append('/').Append(VirtualHost);
+        return new Uri(uriBuilder.ToString());
+    }
 }
